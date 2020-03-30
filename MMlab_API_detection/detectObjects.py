e4db053dcc5ac9551ce0eb97f2bd457dd35a8495
tiennvuit <tiennvuit@gmail.com>
2020-03-30 14:25:48 +0700

    add

--- MMlab_API_detection/detectObjects.py
+++ MMlab_API_detection/detectObjects.py
@@ -28,7 +28,7 @@ def get_boundingBoxes(image_path: str):
         'data': {
             'method': 'yolov3',
             'model_id': '1575949128.9343169',
-            'images': [encoded_string, encoded_string]
+            'images': [encoded_string]
         }
     }
     headers = {'Content-type': 'application/json', 'Authorization': "bearer " + token}
@@ -36,19 +36,23 @@ def get_boundingBoxes(image_path: str):
     response = requests.post(url, data = data_json, headers=headers)
     return response.json()
 
-
 def draw_bounding_boxes(image, bboxes: json):
-    bboxes = bboxes["data"]["predicts"][0] +  bboxes["data"]["predicts"][1]
-    
-
+    # Get the bounding boxes from response got from API
+    bounding_boxes = bboxes['data']['predicts'][0]   # An array contains all bounding boxes of detected object in image.
+    for bounding_box in bounding_boxes:
+        box = bounding_box['bbox']
+        label = bounding_box['object']
+        score = bounding_box['score']
+        #draw bouding box, label, score for each object in image
 
 def main(args):
     image_path = str(args.path)
     # Get bouding boxes of objects in image.
     bboxes = get_boundingBoxes(image_path=image_path)
-    print(bboxes)
+    # print(bboxes['data']['predicts'][0])
     # Draw bounding boxes to image.
-    # draw_bounding_boxes(image=image, bboxes=bboxes)
+    image = Image.open(image_path)
+    draw_bounding_boxes(image=image, bboxes=bboxes)
     
     
 if __name__ == "__main__":
