092013372ebd191839accb52466343bbf8ea11d7
tiennv <tiennvuit@gmail.com>
2020-03-30 23:04:43 +0700

    change some private information

--- Use MMLAB's API/user_login.py
+++ Use MMLAB's API/user_login.py
@@ -9,15 +9,17 @@ def get_token():
     # The user_name, password we need to provided by administrators of MMlab.
     # To get the account (username, password), let contact Mmlab via email:
     #                     mmlab@uit.edu.vn
+    # Use the token providing bellow. 
 
-    data ={'user_name': 'ahihi', 'password': 'ahihi'} # Just a joke
+    data ={'user_name': '*****', 'password': '*****'}
     headers = {'Content-type': 'application/json'}
     data_json = json.dumps(data)
     response = requests.post(url, data = data_json, headers=headers)
     dictionary = response.content.decode()
     index = dictionary.find("token")
     index = index + 8
-    return dictionary[index:-3]
+    # return dictionary[index:-3]
+    return "eyJhbGciOiJIUzUxMiIsImlhdCI6MTU4NTU4MzkyNSwiZXhwIjoxNjE3MTE5OTI1fQ.eyJ1c2VybmFtZSI6ImFkbWluIn0.NZz--dSYal5wgSpUoNYuK5R37kHCnxC8NeKb9oI42kD0P4dagN0PD1XliKInqQGBNJEcZvp1PBsZFboYD09JYg"
 
 def main():
     token = get_token()
